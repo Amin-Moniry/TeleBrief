79b4fbe82cb7abf2bbb4842522d737de05cc1dcf
Amin Moniry <aminmoniry199@gmail.com>
2026-09-12 17:13:14 +0330

    updated

--- digest_core.py
+++ digest_core.py
@@ -55,8 +55,7 @@ AI_CHANNELS = [
     "Hugging_face_news", "samiotech",
 ]
 CHANNELS = SECURITY_CHANNELS
-FOOTER = '<a href="https://t.me/telebriefdata_bot">𝐉𝐎𝐈𝐍</a> ➣ <b>TeleBrief</b>'
-
+FOOTER = '<blockquote>‌<a href="https://t.me/telebriefdata_bot">𝐉𝐎𝐈𝐍</a> ➣ <b>TeleBrief</b></blockquote>'
 
 @dataclass(frozen=True)
 class ChannelMessage:
